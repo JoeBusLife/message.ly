fcf9f49854f327a3f0f98e06c163d5356d87f12a
joebuslife <h.joseph91@hotmail.com>
2021-11-01 23:02:06 -0700

    submit1

---
+++ routes/auth.js
@@ -0,0 +1,56 @@
+const express = require("express");
+
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config")
+const User = require("../models/user");
+const ExpressError = require("../expressError");
+
+const router = new express.Router();
+
+/** POST /login - login: {username, password} => {token}
+ *
+ * Make sure to update their last-login!
+ *
+ **/
+
+router.post("/login", async function(req, res, next) {
+	try {
+		const { username, password } = req.body;
+	
+		if (await User.authenticate(username, password)) {
+			const token = jwt.sign({ username }, SECRET_KEY);
+			User.updateLoginTimestamp(username);
+			return res.json({ token });
+		}
+		throw new ExpressError("Invalid username/password", 400);
+	} catch (e) {
+		return next(e);
+	}
+});
+
+
+/** POST /register - register user: registers, logs in, and returns token.
+ *
+ * {username, password, first_name, last_name, phone} => {token}.
+ *
+ *  Make sure to update their last-login!
+ */
+
+ router.post("/register", async function(req, res, next) {
+	try {
+		let {username} = await User.register(req.body);
+
+		const token = jwt.sign({ username }, SECRET_KEY);
+		User.updateLoginTimestamp(username);
+		return res.json({ token });
+
+	} catch (e) {
+		if (e.code === '23505') {
+      return next(new ExpressError("Username taken. Please pick another!", 400));
+    }
+		return next(e);
+	}
+});
+
+
+module.exports = router;
---
+++ routes/messages.js
@@ -0,0 +1,82 @@
+const express = require("express");
+
+const Message = require("../models/message");
+const {ensureLoggedIn} = require("../middleware/auth");
+const ExpressError = require("../expressError");
+
+const router = new express.Router();
+
+/** GET /:id - get detail of message.
+ *
+ * => {message: {id,
+ *               body,
+ *               sent_at,
+ *               read_at,
+ *               from_user: {username, first_name, last_name, phone},
+ *               to_user: {username, first_name, last_name, phone}}
+ *
+ * Make sure that the currently-logged-in users is either the to or from user.
+ *
+ **/
+
+router.get("/:id", ensureLoggedIn, async function(req, res, next) {
+	try {
+		let message = await Message.get(req.params.id);
+
+		if (message.from_user.username === req.user.username ||
+			message.to_user.username === req.user.username) {
+			return res.json({message});
+		}
+		throw new ExpressError("Not authorized to access this message", 401)
+		
+	} catch (e) {
+		return next(e);
+	}
+});
+
+
+/** POST / - post message.
+ *
+ * {to_username, body} =>
+ *   {message: {id, from_username, to_username, body, sent_at}}
+ *
+ **/
+
+router.post("/", ensureLoggedIn, async function(req, res, next) {
+	try {
+		const {to_username, body} = req.body;
+		let message = await Message.create({
+			from_username: req.user.username, to_username, body});
+
+		return res.json({message});
+	} catch (e) {
+		return next(e);
+	}
+});
+
+
+/** POST/:id/read - mark message as read:
+ *
+ *  => {message: {id, read_at}}
+ *
+ * Make sure that the only the intended recipient can mark as read.
+ *
+ **/
+
+router.post("/:id/read", ensureLoggedIn, async function(req, res, next) {
+	try {
+		let id = req.params.id;
+		let message = await Message.get(id);
+
+		if (message.to_user.username === req.user.username) {
+			return res.json({message: await Message.markRead(id)});
+		}
+		throw new ExpressError("Not authorized to mark as read", 401)
+		
+	} catch (e) {
+		return next(e);
+	}
+});
+
+
+module.exports = router;
